7397eabc867a5c68cee2603e8a90d3bcfaa00664
marcoxavier33 <max.antoine@hotmail.fr>
2022-12-22 19:58:40 +0100

    fix

--- functions/functions.sh
+++ functions/functions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source vars.ini
+source ../vars/vars.ini
 delete_torrent () {
 	PERCENT_DONE=$(curl --silent -H "X-Transmission-Session-Id: $SESSION_ID" $API_URL -d '{"method":"torrent-get","arguments":{"fields":["name","id","percentDone"]}}' | jq -r '.arguments.torrents[] | select(.name=="'"$1"'") | .percentDone')
 	if [[ $PERCENT_DONE -eq 1 ]]; then
